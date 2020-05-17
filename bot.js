7532a8bdcc33bf693073f2ebe2d57a79776b7fdc
calumtebbutt <65409206+calumtebbutt@users.noreply.github.com>
2020-05-17 17:17:33 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -80,6 +80,26 @@ client.on('message', message=>{
 })
 
 
+
+// buses command
+
+client.on('message', message=>{
+
+    let args = message.content.substring(PREFIX.length).split(" ");
+
+    switch(args[0]) {
+        case 'buses':
+            message.author.send(buses);
+        break;
+    }
+})
+
+
+
+
+
+
+
 // all command replies
 
 const rules = ("\n\n**Rules**\n**1.** No Racism\n**2.** No Spamming\n**3.** No begging for ranks\n**4.** No unecessary photos\n**5.** There are chat warnings, they can be given out by seniors and if you have 3 warnings you'll be kicked from the server.\n**6.** No Nudity or NSFW\n**7.** No Homophobic Language\n**8.** No Advertising other groups or games\n**9.** No asking for release dates, when we have a date it will be well publicised unless we don't wish for it to be.\n**10.** No tagging seniors unless for a reason of urgent nature.\n\n------------------------------------------------------------------------------------------------------------\n\n")
@@ -92,6 +112,7 @@ const ratp = ("\n\n**RATP Helper**\n\n**!ratp** - to view all available commands
 
 const meeting = ("\n\n**Weekly Meeting Notes**\n\niiReece98's Birthday Celebration\nMore Previews of London & East V3\nRATP Game Night may be coming up soon\n\n------------------------------------------------------------------------------------------------------------\n\n")
 
+const buses = ("\n\n**RATP Dev London Fleet**\n\nTo be added when reece sends them.\n\n------------------------------------------------------------------------------------------------------------\n\n")
 
 
 
