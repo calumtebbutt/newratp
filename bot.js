04db8266b8a5026c0f60d53570e0529d65421046
calumtebbutt <65409206+calumtebbutt@users.noreply.github.com>
2020-06-16 18:57:12 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -148,7 +148,7 @@ client.on('message', message=>{
     let args = message.content.substring(PREFIX.length).split(" ");
   
     switch(args[0]) {
-        case 'testping':
+        case 'ping':
 
         var facts = [":ping_pong: Pong! took **121ms**", ":ping_pong: Pong! took **125ms**", ":ping_pong: Pong! took **124ms**", ":ping_pong: Pong! took **129ms**", ":ping_pong: Pong! took **118ms**"];
         var fact = Math.floor(Math.random() * facts.length);  
